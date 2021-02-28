6750005f37557f3dc9a63fec6eb125d7e8cc9512
Malakkhanim <70220517+Malakkhanim@users.noreply.github.com>
2021-02-28 17:17:45 +0400

    Add files via upload

---
+++ seasons.py
@@ -0,0 +1,13 @@
+a=int(input())
+if a<=2 :
+    print("Winter")
+if a>2 and a<=5:
+    print("Spring")
+if a>5 and a<=8:
+    print("Summer")
+if a>8 and a<=11:
+    print("Autumn")
+elif a==12:
+    print("Winter")
+else:
+    print("Duzgun-eded-daxil-edin")
